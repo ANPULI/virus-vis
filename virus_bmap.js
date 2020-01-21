8d08684c044477b9c31ceb1cee4f760e8a9b535a
ANPULI <al4902@nyu.edu>
2020-01-22 02:07:53 +0800

    implement visualization

---
+++ virus_bmap.js
@@ -0,0 +1,216 @@
+import echarts from "echarts";
+import bmap from "echarts/extension/bmap/bmap";
+console.log(bmap)
+import geoCoordMap from './assets/geoCoord.json'
+import data from "./assets/provinceInfection.json"
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var province = data[i].name.slice(0, 2)
+        var geoCoord = geoCoordMap[province];
+        if (! geoCoord) {
+            province = data[i].name.slice(0, 3)
+            geoCoord = geoCoordMap[province];
+        }
+        if (geoCoord) {
+            res.push({
+                name: province,
+                value: geoCoord.concat(data[i].value)
+            });
+        }
+    }
+    return res;
+};
+console.log(convertData(data));
+console.log(convertData(data).sort(function (a, b) {
+    return b.value[2] - a.value[2];
+}).slice(0, 6));
+
+var myChart = echarts.init(document.getElementById('main'));
+
+
+var option = {
+    title :{
+        text: "全国新型肺炎疫情实时动态 - 2020/1/22",
+        subtext: "数据来源：澎湃新闻 & 丁香医生",
+        sublink: "https://mp.weixin.qq.com/s/-16yyC-KXx6Od7MCHd51Tw",
+        left: "center"
+    },
+    tooltip: {
+        trigger: 'item',
+        showDelay: 0,
+        transitionDuration: 0.2,
+        formatter: function (params) {
+            return params.seriesName + ' - ' + params.name + '<br/>' + '确诊：' + params.value[2] + '<br/>' + '疑似：' + params.value[3];
+        }
+    },
+    bmap: {
+        center: [114.114129, 32.550339],
+        zoom: 6,
+        roam: true,
+        mapStyle: {
+            styleJson: [{
+                'featureType': 'water',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#d1d1d1'
+                }
+            }, {
+                'featureType': 'land',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#f3f3f3'
+                }
+            }, {
+                'featureType': 'railway',
+                'elementType': 'all',
+                'stylers': {
+                    'visibility': 'off'
+                }
+            }, {
+                'featureType': 'highway',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#fdfdfd'
+                }
+            }, {
+                'featureType': 'highway',
+                'elementType': 'labels',
+                'stylers': {
+                    'visibility': 'off'
+                }
+            }, {
+                'featureType': 'arterial',
+                'elementType': 'geometry',
+                'stylers': {
+                    'color': '#fefefe'
+                }
+            }, {
+                'featureType': 'arterial',
+                'elementType': 'geometry.fill',
+                'stylers': {
+                    'color': '#fefefe'
+                }
+            }, {
+                'featureType': 'poi',
+                'elementType': 'all',
+                'stylers': {
+                    'visibility': 'off'
+                }
+            }, {
+                'featureType': 'green',
+                'elementType': 'all',
+                'stylers': {
+                    'visibility': 'off'
+                }
+            }, {
+                'featureType': 'subway',
+                'elementType': 'all',
+                'stylers': {
+                    'visibility': 'off'
+                }
+            }, {
+                'featureType': 'manmade',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#d1d1d1'
+                }
+            }, {
+                'featureType': 'local',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#d1d1d1'
+                }
+            }, {
+                'featureType': 'arterial',
+                'elementType': 'labels',
+                'stylers': {
+                    'visibility': 'off'
+                }
+            }, {
+                'featureType': 'boundary',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#fefefe'
+                }
+            }, {
+                'featureType': 'building',
+                'elementType': 'all',
+                'stylers': {
+                    'color': '#d1d1d1'
+                }
+            }, {
+                'featureType': 'label',
+                'elementType': 'labels.text.fill',
+                'stylers': {
+                    'color': '#999999'
+                }
+            }]
+        }
+    },
+    toolbox: {
+        show: true,
+        //orient: 'vertical',
+        left: 'left',
+        top: 'top',
+        feature: {
+            dataView: {readOnly: false},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    series : [
+        {
+            name: '影响人数',
+            type: 'scatter',
+            coordinateSystem: 'bmap',
+            data: convertData(data),
+            symbolSize: function (val) {
+                return 30 * Math.log10(val[2] + val[3] + 1);
+            },
+            label: {
+                formatter: '{b}',
+                position: 'right',
+                show: false
+            },
+            itemStyle: {
+                color: 'purple'
+            },
+            emphasis: {
+                label: {
+                    show: true
+                }
+            }
+        },
+        {
+            name: 'Top 5',
+            type: 'effectScatter',
+            coordinateSystem: 'bmap',
+            data: convertData(data).sort(function (a, b) {
+                return b.value[2] - a.value[2];
+            }).slice(0, 6),
+            symbolSize: function (val) {
+                return 30 * Math.log10(val[2] + val[3] / 5 + 1);
+            },
+            showEffectOn: 'render',
+            rippleEffect: {
+                brushType: 'stroke'
+            },
+            hoverAnimation: true,
+            label: {
+                formatter: '{b}',
+                position: 'right',
+                show: true
+            },
+            itemStyle: {
+                color: 'purple',
+                shadowBlur: 10,
+                shadowColor: '#333'
+            },
+            zlevel: 1
+        }]
+};
+
+// console.log(myChart)
+myChart.setOption(option);
--- virus_visualmap.js
+++ virus_visualmap.js
@@ -1,6 +1,6 @@
 import echarts from "echarts";
 import mapJson from './assets/map.geo.json'
-import provinceDefault from "./assets/provinceDefault.json"
+import provinceInfection from "./assets/provinceInfection.json"
 // window.map = mapJson
 // window.pd = provinceDefault
 // console.log(mapJson)
@@ -26,6 +26,10 @@ import provinceDefault from "./assets/provinceDefault.json"
 var convertData = function (data) {
     var res = [];
     for (var i = 0; i < data.length; i++) {
+        res.push({
+            name: data[i].name,
+            value: data[i].value.reduce((a, b) => a + b, 0)
+        });
         var geoCoord = mapJson[data[i].name];
         if (geoCoord) {
             res.push({
@@ -63,13 +67,13 @@ var option = {
     },
     visualMap: {
         min: 0,
-        max: 100000,
+        max: 300,
         text: ['High', 'Low'],
         left: 'right',
-        realtime: false,
+        realtime: true,
         calculable: true,
         inRange: {
-            color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+            color: ['#FFFFFF', '#0000FF', '#FFFF00', '#FFA500', '#FF0000']
         }
     },
     toolbox: {
@@ -85,21 +89,21 @@ var option = {
     },
     series: [
         {
-        name: '疫情人数',
+        name: '影响人数',
         type: 'map',
         mapType: 'MY',
         roam: true,
-        data: provinceDefault,
+        data: convertData(provinceInfection),
         aspectScale: 0.85, //地图长度比
         label: {
             normal: {
-                show: false,
+                show: true,
                 textStyle: {
                     color: '#000'
                 }
             },
             emphasis: {
-                show: true,
+                show: false,
                 textStyle: {
                     color: '#333'
                 }
